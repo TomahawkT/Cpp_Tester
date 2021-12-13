90edefa95d0824ad399886456da1937f87025f21
Tommy <tmmymrtnz@fugaku-v20.lan>
2021-12-13 18:33:07 -0300

    Trying public and private variables, with implementation of getter and setter funcions

--- classTest.cpp
+++ classTest.cpp
@@ -2,24 +2,43 @@
 using namespace std;
 
 class Book{
+    private:
+        int pages;
     public:
         string title;
         string author;
-        int pages;
 
         Book(string bookTitle, string bookAuthor, int bookPages){
             title = bookTitle;
             author = bookAuthor;
-            pages = bookPages;
+            setPages(bookPages);
             cout << "Book created" << endl;
         }
+
+        void setPages(int bookPages)
+        {
+            if (bookPages > 0)
+                pages = bookPages;
+            else
+                pages = 0;
+        }
+        bool isLong(){
+            if (pages > 500){
+                return 1;
+            }
+            return 0;
+        }
+        int getPages(void){
+            return pages;
+        }
+    
 };
 
 int main (void)
 {
     Book book1("Harry Potter", "J.K. Rowling", 516);
-    Book book2("Lord of The Rings", "Tolkien", 739);
+    Book book2("Lord of The Rings", "Tolkien", -400);
 
-    cout << book2.author <<endl;
+    cout << book2.getPages() <<endl;
     return 0;
 }
